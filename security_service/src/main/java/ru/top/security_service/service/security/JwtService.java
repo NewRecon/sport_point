902ba42303b5ff5c0941cb9e71a69fd9d10c1470
mshabanov <mshabanov@zuzex.com>
2025-05-19 17:06:50 +0300

    implement base realization JwtService

--- security_service/src/main/java/ru/top/security_service/service/security/JwtService.java
+++ security_service/src/main/java/ru/top/security_service/service/security/JwtService.java
@@ -1,5 +1,88 @@
 package ru.top.security_service.service.security;
 
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+import io.jsonwebtoken.Claims;
+
 public interface JwtService {
 
+    /**
+     * Генерация токена
+     *
+     * @param userDetails данные пользователя
+     * @return токен
+     */
+    String generateToken(UserDetails userDetails);
+
+    /**
+     * Генерация токена с дополнительными данными
+     *
+     * @param extraClaims дополнительные данные
+     * @param userDetails данные пользователя
+     * @return токен
+     */
+    String generateToken(Map<String, Object> extraClaims, UserDetails userDetails);
+
+    /**
+     * Проверка токена на валидность
+     *
+     * @param token       токен
+     * @param userDetails данные пользователя
+     * @return true, если токен валиден
+     */
+    boolean isTokenValid(String token, UserDetails userDetails);
+
+    /**
+     * Проверка токена на просроченность
+     *
+     * @param token токен
+     * @return true, если токен просрочен
+     */
+    boolean isTokenExpired(String token);
+
+    /**
+     * Извлечение данных из токена
+     *
+     * @param token           токен
+     * @param claimsResolvers функция извлечения данных
+     * @param <T>             тип данных
+     * @return данные
+     */
+    <T> T extractClaim(String token, Function<Claims, T> claimsResolvers);
+
+    /**
+     * Извлечение имени пользователя из токена
+     *
+     * @param token токен
+     * @return имя пользователя
+     */
+    String extractUserName(String token);
+
+    /**
+     * Извлечение даты истечения токена
+     *
+     * @param token токен
+     * @return дата истечения
+     */
+    Date extractExpiration(String token);
+
+    /**
+     * Извлечение всех данных из токена
+     *
+     * @param token токен
+     * @return данные
+     */
+    Claims extractAllClaims(String token);
+
+    /**
+     * Получение ключа для подписи токена
+     *
+     * @return ключ
+     */
+    Key getSigningKey();
 }
--- security_service/src/main/java/ru/top/security_service/service/security/impl/JwtServiceImpl.java
+++ security_service/src/main/java/ru/top/security_service/service/security/impl/JwtServiceImpl.java
@@ -1,7 +1,74 @@
 package ru.top.security_service.service.security.impl;
 
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import ru.top.security_service.service.security.JwtService;
 
 public class JwtServiceImpl implements JwtService {
+
+    @Value("${token.signing.key}")
+    private String jwtSigningKey;
+
+    @Override
+    public String generateToken(UserDetails userDetails) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'generateToken'");
+    }
+
+    @Override
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'generateToken'");
+    }
+
+    @Override
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isTokenValid'");
+    }
+
+    @Override
+    public boolean isTokenExpired(String token) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isTokenExpired'");
+    }
+
+    @Override
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolvers) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolvers.apply(claims);
+    }
+
+    @Override
+    public String extractUserName(String token) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'extractUserName'");
+    }
+
+    @Override
+    public Date extractExpiration(String token) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'extractExpiration'");
+    }
+
+    @Override
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser().decryptWith(getSigningKey()).build().parseSignedClaims(token).getPayload();
+    }
+
+    @Override
+    public SecretKey getSigningKey() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSigningKey'");
+    }
 
 }
